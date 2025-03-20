a7295dea70dcb0ae5d52696b33ce74ec5a2ce052
X104n <github.freeware171@passmail.net>
2025-03-20 15:18:19 +0100

    Client code added

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(('84:1b:77:a4:ed:c6', 4))
+
+try:
+    while True:
+        message = input("")
+        client.send(message.encode())
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message: {data.decode('utf-8')}")
+
+except OSError as e:
+    pass
+
+client.close()
