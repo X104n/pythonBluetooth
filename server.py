56f9bfb1d6c8611e89b692050c78dd147fabefe7
X104n <github.freeware171@passmail.net>
2025-03-20 14:50:53 +0100

    Added ignore file, and server file

--- server.py
+++ server.py
@@ -1,3 +1,21 @@
 import socket
 
-socket.socket()
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server.bind(("84:1b:77:a4:ed:c6", 4))
+server.listen(1)
+
+client, address = server.accept()
+
+try:
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message: {data.decode('utf-8')}")
+        message = input("Enter message:")
+        client.send(message.encode('utf-8'))
+except OSError as e:
+    pass
+
+client.close()
+server.close()
